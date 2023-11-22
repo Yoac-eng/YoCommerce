e5ef7f995be2f7cce69d3e5f800c7cd5e956e022
Yoac <cayocutrim1991@gmail.com>
2023-11-21 23:45:19 -0300

    empty project

---
+++ src/main/java/com/yoaceng/yocommerce/YocommerceApplication.java
@@ -0,0 +1,13 @@
+package com.yoaceng.yocommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YocommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YocommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoaceng/yocommerce/YocommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoaceng.yocommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YocommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
